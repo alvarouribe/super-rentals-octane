12e4d578ea17770f8e2a075f5ee050c6a2682b7f
Alvaro Uribe <iam@alvarouribe.cl>
2019-09-25 15:17:15 +1200

    Added 3 acceptance tests

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -1,13 +1,50 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | super rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /super-rentals', async function(assert) {
-    await visit('/super-rentals');
+  test('visiting /', async function(assert) {
+    await visit('/');
 
-    assert.equal(currentURL(), '/super-rentals');
+    //Make sure we are in the index page
+    assert.equal(currentURL(), '/');
+
+    //Make sure we can see the header of this page
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+
+    //Make sure we have a button with text 'About Us'
+    assert.dom('.jumbo a.button').hasText('About Us');
+
+    //Let's click on it with the click helper
+    await click('.jumbo a.button');
+
+    //Make sure we are in the about page
+    assert.equal(currentURL(), '/about');
+  });
+
+  test('visiting /about', async function(assert) {
+    await visit('/about');
+
+    assert.equal(currentURL(), '/about');
+    assert.dom('h2').hasText('About Super Rentals');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting /getting-in-touch', async function(assert) {
+    await visit('/getting-in-touch');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('a.button').hasText('About');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
   });
 });
